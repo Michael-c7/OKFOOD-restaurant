6f986a989e63369985a6f7e603b3a3713aeb8cd0
Michael Carr <mcarr2468@gmail.com>
2021-08-18 19:05:09 -0500

    V1.2 finished header & navbar sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.scss';
+// images
+    // header
+        import headerImage1 from "./images/header/pexels-kasumi-loffler-3535395.jpg"
+        import headerImage2 from "./images/header/pexels-lisa-1279330.jpg";
+        import headerImage3 from "./images/header//pexels-roger-cziwerny-792028.jpg";
+    // about
+        import aboutImage1 from "./images/about/divani-diva-s0duM7x1yuA-unsplash_a.png"
+    // popular-dish
+        import popularDish1 from "./images/popular-dish/amirali-mirhashemian-jh5XyK4Rr3Y-unsplash_a.png";
+        import popularDish2 from "./images/popular-dish/homescreenify-sA3wymYqyaI-unsplash_a.png";
+        import popularDish3 from "./images/popular-dish/lynda-hinton-77RJFb0IXnQ-unsplash_a.png";
+    // our-menu
+        import ourMenuImage1 from "./images/our-menu/alexandru-bogdan-ghita-UeYkqQh4PoI-unsplash.jpg";
+        import ourMenuImage2 from "./images/our-menu/melissa-walker-horn-gtDYwUIr9Vg-unsplash.jpg";
+        import ourMenuImage3 from "./images/our-menu/patrick-le-6HvDmcA0W1w-unsplash.jpg";
+        import ourMenuImage4 from "./images/our-menu/proriat-hospitality-lwoTuByIuC4-unsplash.jpg";
+        import ourMenuImage5 from "./images/our-menu/vitalii-chernopyskyi-Oxb84ENcFfU-unsplash.jpg";
+    // testimonial
+        import testimonialImage1 from "./images/testimonial/jake-nackos-IF9TK5Uy-KI-unsplash.jpg";
+        import testimonialImage2 from "./images/testimonial/luis-villasmil-hh3ViD0r0Rc-unsplash.jpg";
+
+// import js here
